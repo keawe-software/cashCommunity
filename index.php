613c916fbd94a270d68609b9ecbfd5a248aefafd
Stephan Richter <srichter@srsoftware.de>
2014-10-20 00:40:19 +0200

    started writing room functions

--- index.php
+++ index.php
@@ -9,14 +9,18 @@ if (isset($_POST['flatmate'])){
 } else if (isset($_POST['newflatmate'])){
 	$data=addFlatmate($_POST['newflatmate']);
 	include 'templates/flatmate_man.php';
-} else if (isset($_POST['manage_invoices'])){
-	include 'templates/invoice_man.php';
 } else if (isset($_POST['manage_flatmates'])){
 	$data=getData();
 	include 'templates/flatmate_man.php';
+} else if (isset($_POST['manage_invoices'])){
+	include 'templates/invoice_man.php';
 } else if (isset($_POST['manage_distributions'])){
 	include 'templates/distribution_man.php';
+} else if (isset($_POST['room'])){
+  $data=editRoom($_POST['room']);
+  include 'templates/room_man.php';
 } else if (isset($_POST['manage_rooms'])){
+  $data=getData();
 	include 'templates/room_man.php';
 } else {
 	include 'templates/overview.php';	
--- templates/room_man.php
+++ templates/room_man.php
@@ -1,3 +1,32 @@
+<table>
+  <tr>
+    <th><?php print t('Id'); ?></th>
+    <th><?php print t('Name'); ?></th>
+    <th><?php print t('Size'); ?></th>
+    <th><?php print t('Action'); ?></th>
+  </tr>
 <?php
-?>
 
+foreach ($data['rooms'] as $room){
+	?>
+	<form action="." method="POST">
+	<tr>
+	  <td><input type="hidden" name="room[id]" value="<?php print $room['id'];?>"/><?php print $room['id'];?></td>
+	  <td><input type="text" name="room[name]" value="<?php print $room['name'];?>"/></td>
+		<td><input type="text" name="room[size]" value="<?php print $room['size'];?>"/></td>
+		<td><input type="submit"/></td>
+	</tr>
+	</form>
+	<?php 
+}
+?>
+	<form action="." method="POST">
+	<tr>
+	  <td></td>
+	  <td><input type="text" name="newroom[name]"/></td>
+		<td><input type="text" name="newroom[size]"/></td>
+		<td><input type="submit"/></td>
+	</tr>
+	</form>
+
+</table>
