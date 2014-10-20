83ade731abf75f770794b74d3b2e53f0cbe20fa0
Stephan Richter <github@srsoftware.de>
2014-10-20 22:44:38 +0200

    changed overview menu

--- templates/overview.php
+++ templates/overview.php
@@ -1,8 +1,9 @@
 <form action="." method="post">
 <ul class="menu">
-  <li><input type="submit" name="manage_invoices" value="<?php print t('manage_invoices'); ?>"/></li>
-  <li><input type="submit" name="manage_flatmates" value="<?php print t('manage_flatmates'); ?>"/></li>
-  <li><input type="submit" name="manage_distributions" value="<?php print t('manage_distributions'); ?>"/></li>
   <li><input type="submit" name="manage_rooms" value="<?php print t('manage_rooms'); ?>"/></li>
-</ul>
+  <li><input type="submit" name="manage_flatmates" value="<?php print t('manage_flatmates'); ?>"/></li>
+  <li><input type="submit" name="manage_room_assiciations" value="<?php print t('manage room associations')?>"/></li>
+  <li><input type="submit" name="manage_distributions" value="<?php print t('manage_distributions'); ?>"/></li>
+	<li><input type="submit" name="manage_invoices" value="<?php print t('manage_invoices'); ?>"/></li>
+  </ul>
 </form>
