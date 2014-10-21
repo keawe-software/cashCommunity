2052622807e5736ff4d518c14ffc94cd83223df0
Stephan Richter <github@srsoftware.de>
2014-10-21 10:56:30 +0200

    started refacturing distribution manager

--- templates/distribution_man.php
+++ templates/distribution_man.php
@@ -1,48 +1,27 @@
 <table>
   <tr>
     <th><?php print t('Id'); ?></th>
-    <th><?php print t('Start'); ?></th>
-    <th><?php print t('End'); ?></th>
-    <?php foreach ($base_dist as $mate_id => $percentage){ ?>
-      <th><?php print $data['flatmates'][$mate_id]['name']; ?></th>
+  	<th><?php print t('Name'); ?></th>
+    <?php foreach ($data['rooms'] as $room) {?>
+    <th><?php print $room['name']; ?></th>
     <?php } ?>
     <th><?php print t('Action'); ?></th>
   </tr>
-<?php
-
-if (isset($data)){
-	if (isset($data['flatmates'])){
-    foreach ($data['distributions'] as $id => $distribution){ ?>
-<form action="." method="POST">
+  
+  <?php foreach ($data['distributions'] as $distribution) { ?>
   <tr>
-    <td><input type="hidden" name="distribution[id]" value="<?php print $id; ?>"/><?php print $id; ?></td>
-	  <td><input type="text" name="distribution[from]" value="<?php print $distribution['from'];?>"/></td>
-	  <td><input type="text" name="distribution[till]" value="<?php print $distribution['till'];?>"/></td>
-    <?php foreach ($distribution['rooms'] as $mate_id => $percentage){ ?>
-      <td><input type="text" name="distribution[rooms][<?php print $mate_id; ?>]" value="<?php print $percentage; ?>"/></td>
+    <td><?php print $distribution['id']; ?></td>
+  	<td><?php print $distribution['name']; ?></td>
+    <?php foreach ($data['rooms'] as $room_id => $room) {?>
+    <td><?php
+    if (isset($distribution['rooms'][$room_id])){ 
+    	print $distribution['rooms'][$room_id];
+		} else {
+  		print '0.0';
+		}?>%</td>
     <?php } ?>
-    <td><input type="submit" /></td>
-	</tr>
-  </form>
-	<?php 
-} // foreach
-} // if
-} // if
-?>
-<form action="." method="POST">
-  <tr>
-    <td><?php print t('new'); ?></td>
-    <td><input type="text" name="newdistribution[from]" value="<?php print date('Y-m-d'); ?>"/></td>
-    <td><input type="text" name="newdistribution[till]" value="<?php print date('Y-m-d'); ?>"/></td>
-    <?php foreach ($base_dist as $id => $percentage) { ?>
-      <td><input type="text" name="newdistribution[rooms][<?php print $id; ?>]" value="<?php print $percentage; ?>"/></td>
-    <?php } ?>
-    <td><input type="submit" /></td>
-  </tr>
-  </form>
+    <td><?php print t('Action'); ?></td>
+  </tr>  
+  <?php } ?>
+  
 </table>
-<form action="." method="post">
-<ul class="menu">
-  <li><input type="submit" name="home" value="<?php print t('home');?>"/></li>
-</ul>
-</form>
