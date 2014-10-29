f89d3bc3b15c262bc09b078af974a9eb2ef7003f
Stephan Richter <s.richter@srsoftware.de>
2014-10-29 20:11:47 +0100

    changed bubble width in login man

--- templates/login_man.php
+++ templates/login_man.php
@@ -5,9 +5,9 @@
 <?php print t('Not registered, yet? Get a login!'); ?>
   <button type="submit" name="action" value="register"><?php print t('register now'); ?></button>
 </form>
-<div class="rbubble" style="width: 80%;">
+<div class="rbubble" style="width: 850px;">
 <?php print t('cashCommunity is a web service allowing you to manage your living communitie\'s invoices and payments. All your data is password protected. We will neither use your data for any purpose other than helping you with calculations, nor will we forward yout data to any third parties.');?>
 </div>
-<div class="rbubble" style="width: 80%;">
+<div class="rbubble" style="width: 850px;">
 <?php print t('This software is open source. However, it is not completely free:<br/>For a period of 14 days you can test its features freely. After that time, you will be no longer able to edit any data. To continue editing, you will have to <a href="license.php">purchase a license</a>.');?>
 </div>
