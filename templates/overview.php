57c3f9a6c8052942cb42f741de5c5ae1471f3313
Stephan Richter <github@srsoftware.de>
2014-10-29 14:53:31 +0100

    added explanation bubbles to overview

--- templates/overview.php
+++ templates/overview.php
@@ -1,8 +1,19 @@
 <form action="." method="post">
-<ul class="menu">
-  <li><button type="submit" name="action" value="manage rooms"><?php print t('manage rooms'); ?></button></li>
-  <li><button type="submit" name="action" value="manage flatmates"><?php print t('manage flatmates'); ?></button></li>
-  <li><button type="submit" name="action" value="manage distributions"><?php print t('manage distributions'); ?></button></li>
-	<li><button type="submit" name="action" value="manage invoices"><?php print t('manage invoices'); ?></button></li>
-  </ul>
+
+<table class="overview">  
+  <tr>
+		<td><button type="submit" name="action" value="manage rooms"><?php print t('manage rooms'); ?></button></td>  	
+  	<td><button type="submit" name="action" value="manage flatmates"><?php print t('manage flatmates'); ?></button></td>
+  	<td><button type="submit" name="action" value="manage distributions"><?php print t('manage distributions'); ?></button></td>
+		<td><button type="submit" name="action" value="manage invoices"><?php print t('manage invoices'); ?></button></td>
+	</tr>
+	<?php if (!isset($_POST['action'])) {?>
+	<tr>
+	  <td class="bubble"><?php print t('Here, you should maintain a list of the rooms of your flat. Each room gan be given a name and a size.'); ?></td>
+	  <td class="bubble"><?php print t('Here, you should maintain the list your flat mates. Each flatmate will have his/her own list of payments and a balance sheet.'); ?></td>
+	  <td class="bubble"><?php print t('This allows you to create various distributions for bills invoiced. Each distribution defines how a certain invoice is shared by your flat mates.'); ?></td>
+	  <td class="bubble"><?php print t('Manage a list of the invoices related to your living community.'); ?></td>
+	</tr>
+	<?php } // if ?>
+</table>
 </form>
