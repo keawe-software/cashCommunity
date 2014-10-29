bf631372da3eca736b1d146e4971ca2b527230e8
Stephan Richter <github@srsoftware.de>
2014-10-29 17:26:08 +0100

    added version 1.0

--- templates/foot.php
+++ templates/foot.php
@@ -8,6 +8,6 @@
     </ul>
     <?php } ?>
     <br/>
-    <div class="footline"><a href="https://github.com/SRSoftware/cashCommunity"><?php print t('This software is open source. Find the code at GitHub.')?></a></div>
+    <div class="footline"><a href="https://github.com/SRSoftware/cashCommunity"><?php print t('This software is open source. Find the code at GitHub.')?></a> Version 1.0</div>
 </body>
 </html>
